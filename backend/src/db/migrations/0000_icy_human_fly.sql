c77c53c3431102e80e3a48c2dcdf7f9266277410
NIDNASSER abdelmajid <abdelmajidnidnasser1@gmail.com>
2025-09-21 21:21:37 +0100

    update db configuration

--- backend/src/db/migrations/0000_icy_human_fly.sql
+++ backend/src/db/migrations/0000_icy_human_fly.sql
@@ -1,7 +1,7 @@
 CREATE TABLE "favorites" (
 	"id" serial PRIMARY KEY NOT NULL,
 	"user_id" text NOT NULL,
-	"recipe_id" text NOT NULL,
+	"recipe_id" integer NOT NULL,
 	"title" text NOT NULL,
 	"image" text,
 	"cook_time" text,
--- backend/src/db/schema.js
+++ backend/src/db/schema.js
@@ -1,9 +1,9 @@
- import {pgTable, serial, text, timestamp, interger} from "drizzle-orm/pg-core";
+ import {pgTable, serial, text, timestamp, integer} from "drizzle-orm/pg-core";
 
  export const favoritsTable = pgTable("favorites", {
     id: serial('id').primaryKey(),
     userId: text('user_id').notNull(),
-    recipeId: text('recipe_id').notNull(),
+    recipeId: integer('recipe_id').notNull(),
     title: text('title').notNull(),
     image: text('image'),
     cookTime: text('cook_time'),

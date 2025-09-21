f457a2875d514afa77a2e5bffca3fc6a74fe3eef
NIDNASSER abdelmajid <abdelmajidnidnasser1@gmail.com>
2025-09-21 20:31:31 +0100

    endpoint and db configured

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,63 @@
 import express from 'express';
 import { ENV } from './config/env.js';
+import { db } from './config/db.js';
+import { favoritsTable } from './db/schema.js';
 
 const app = express();
 const PORT = ENV.PORT || 5001;
 
+app.use(express.json());
+
 app.get('/api/health', (req, res) => {
   res.status(200).json({success:true});
+});
+
+app.post("/api/favorites", async (req, res) => {
+  try {
+    const { userId, recipeId, title, image, cookTime, servings} = req.body;
+
+    if (!userId || !recipeId || !title) {
+      return res.status(400).json({ error: "Missing required fields" });
+    }
+
+    const newFavorite = await db.insert(favoritsTable).values({
+      userId, recipeId, title, image, cookTime, servings
+    }).returning();
+
+    res.statusMessage(201).json(newFavorite[0]);
+  } catch (error) {
+    console.error("Error adding favorite:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.delete("/api/favorites/:userId/:recipeId", async (req, res) => {
+  try {
+    const { userId, recipeId } = req.params;
+
+    await db.delete(favoritsTable).where(
+      and(eq(favoritsTable.userId, userId), eq(favoritsTable.recipeId, parseInt(recipeId)))
+    )
+
+    res.status(200).json({ message: "Favorite deleted successfully" });
+
+  } catch (error) {
+    console.error("Error deleting favorite:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.get("/api/favorites/:userId", async (req, res) => { 
+  try {
+    const { userId } = req.params;
+
+    const userFavorites = await db.select().from(favoritsTable).where(eq(favoritsTable.userId, userId));
+
+    res.status(200).json(userFavorites);
+  } catch (error) {
+    console.error("Error fetching favorites:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
 });
 
 app.listen(PORT, () => {
